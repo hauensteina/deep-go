20b7bd06c80e28b8e9387d41925b405565345ec9
Andreas Hauenstein <hauensteina@gmail.com>
2019-03-04 15:45:07 -0800

    bot_bot_99 switch to fast goboard

--- bot_bot_99_server.py
+++ bot_bot_99_server.py
@@ -9,9 +9,9 @@
 # Start a web interface for bot play
 #
 
-from dlgo.agent.naive import RandomBot
+from dlgo.agent.naive_fast import FastRandomBot
 from dlgo.httpfrontend.server import get_web_app
 
-random_agent = RandomBot()
+random_agent = FastRandomBot()
 web_app = get_web_app({'random':random_agent})
 web_app.run()

5fd08cc289d3372e330bc7ff6c7f7ff2aecb4c8a
Andreas Hauenstein <hauensteina@gmail.com>
2019-03-04 10:47:35 -0800

    bot_bot_99_server.py bot plays itself

---
+++ bot_bot_99_server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# /********************************************************************
+# Filename: bot_v_bot.py
+# Author: AHN
+# Creation Date: Mar, 2019
+# **********************************************************************/
+#
+# Start a web interface for bot play
+#
+
+from dlgo.agent.naive import RandomBot
+from dlgo.httpfrontend.server import get_web_app
+
+random_agent = RandomBot()
+web_app = get_web_app({'random':random_agent})
+web_app.run()

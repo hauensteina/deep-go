ef5c613fd3b57c076f19427a303062c52c3caac9
Andreas Hauenstein <hauensteina@gmail.com>
2019-03-07 14:33:58 -0800

    score19 upload sgf works

--- dlgo/httpfrontend/server.py
+++ dlgo/httpfrontend/server.py
@@ -16,11 +16,13 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
+from dlgo.gotypes import Player, Point
 from dlgo import agent
 from dlgo import goboard_fast as goboard
 from dlgo.utils import coords_from_point
 from dlgo.utils import point_from_coords
 from dlgo.scoring import compute_game_result
+from dlgo.gosgf import Sgf_game
 
 __all__ = [
     'get_web_app',
@@ -56,7 +58,7 @@ def get_web_app(bot_map):
     def select_move(bot_name):
         content = request.json
         board_size = content['board_size']
-        game_state = goboard.GameState.new_game(board_size)
+        game_state = goboard.GameState.new_game( board_size)
         # Replay the game up to this point.
         for move in content['moves']:
             if move == 'pass':
@@ -64,16 +66,16 @@ def get_web_app(bot_map):
             elif move == 'resign':
                 next_move = goboard.Move.resign()
             else:
-                next_move = goboard.Move.play(point_from_coords(move))
-            game_state = game_state.apply_move(next_move)
+                next_move = goboard.Move.play( point_from_coords(move))
+            game_state = game_state.apply_move( next_move)
         bot_agent = bot_map[bot_name]
-        bot_move = bot_agent.select_move(game_state)
+        bot_move = bot_agent.select_move( game_state)
         if bot_move is None or bot_move.is_pass:
             bot_move_str = 'pass'
         elif bot_move.is_resign:
             bot_move_str = 'resign'
         else:
-            bot_move_str = coords_from_point(bot_move.point)
+            bot_move_str = coords_from_point( bot_move.point)
         return jsonify({
             'bot_move': bot_move_str,
             'diagnostics': bot_agent.diagnostics()
@@ -97,6 +99,46 @@ def get_web_app(bot_map):
             game_state = game_state.apply_move( next_move)
 
         res = compute_game_result( game_state)
+        return jsonify( {'result': res})
+
+    @app.route('/sgf2list', methods=['POST'])
+    # Convert sgf main var to coordinate list of moves
+    #----------------------------------------------------
+    def sgf2list():
+        f = request.files['file']
+        sgfstr = f.read()
+        sgf = Sgf_game.from_string( sgfstr)
+        res = []
+
+        #------------------------
+        def move2coords( move):
+            row, col = move
+            p = Point( row + 1, col + 1)
+            coords = coords_from_point( p)
+            return coords
+
+        # Deal with handicap in the root node
+        if sgf.get_handicap() is not None and sgf.get_handicap() != 0:
+            for setup in sgf.get_root().get_setup_stones():
+                for idx, move in enumerate( setup):
+                    if idx > 0: res.append( 'pass')
+                    res.append( move2coords( move))
+
+        # Nodes in the main sequence
+        for item in sgf.main_sequence_iter():
+            color, move_tuple = item.get_move()
+            point = None
+            if color is not None:
+                if move_tuple is not None:
+                    res.append( move2coords( move_tuple))
+                else:
+                    res.append( 'pass')
+            # Deal with handicap stones as individual nodes
+            elif item.get_setup_stones()[0]:
+                move = list( item.get_setup_stones()[0])[0]
+                if res: res.append( 'pass')
+                res.append( move2coords( move))
+
         return jsonify( {'result': res})
 
     return app

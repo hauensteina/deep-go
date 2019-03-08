3226163710ae7659130f239cc3731e5c23e6107f
Andreas Hauenstein <hauensteina@gmail.com>
2019-03-08 15:52:42 -0800

    SmartRandomBot escapes atari

--- score19_server.py
+++ score19_server.py
@@ -9,11 +9,12 @@
 # Start a web interface to test 19x19 scoring
 #
 
-from dlgo.agent.naive_fast import FastRandomBot
+#from dlgo.agent.naive_fast import FastRandomBot
+from dlgo.agent.naive_fast_smart import FastSmartRandomBot
 from dlgo import mcts
 from dlgo.httpfrontend.server import get_web_app
 
 print( 'Point your browser at http://127.0.0.1:5000/static/score19.html')
-random_agent = FastRandomBot()
-web_app = get_web_app({'random':random_agent})
+smart_random_agent = FastSmartRandomBot()
+web_app = get_web_app({'smartrandom':smart_random_agent})
 web_app.run()

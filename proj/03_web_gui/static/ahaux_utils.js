c51b4656b3570751b37b32db750063893c19f361
Andreas Hauenstein <hauensteina@gmail.com>
2019-04-26 15:35:52 +0000

    add a barchart to ahaux_utils.js

--- proj/03_web_gui/static/ahaux_utils.js
+++ proj/03_web_gui/static/ahaux_utils.js
@@ -15,19 +15,26 @@ class AhauxUtils
     this.$ = $
   } // contructor()
 
+  //----------------------------
+  //--- D3 graphics routines ---
+  //----------------------------
+
   // Simple line chart using d3.
   // container is a string like '#some_div_id'.
-  //------------------------------------------------
-  plot_line( container, data, xlim, ylim) {
+  // data looks like [[x_0,y_0], ... ] .
+  // xlim and ylim are pairs like [x_min,x_max] .
+  //----------------------------------------------
+  plot_line( container, data, xlim, ylim, color) {
+    color = color || 'steelblue'
     var [d3,$] = [this.d3, this.$]
     var C = d3.select( container)
     $(container).html('')
-    var width  = $(container).width()
-    var height = $(container).height()
+    var w  = $(container).width()
+    var h = $(container).height()
 
     var margin = {top: 50, right: 50, bottom: 50, left: 50}
-      , width = width - margin.left - margin.right
-      , height = height - margin.top - margin.bottom
+      ,width = w - margin.left - margin.right
+      ,height = h - margin.top - margin.bottom
 
     var scale_x = d3.scaleLinear()
       .domain([xlim[0], xlim[1]]) // input
@@ -64,10 +71,77 @@ class AhauxUtils
     // Draw the line
     svg.append('path')
       .datum(data) // Binds data to the line
-      .attr('style', 'fill:none;stroke:#ffab00;stroke-width:3')
+      .attr('style', 'fill:none;stroke:' + color + ';stroke-width:3')
       .attr('d', line) // Call the line generator
 
   } // plot_line()
+
+  // Barchart.
+  // container is a string like '#some_div_id'.
+  // data looks like [[x_0,y_0], ... ] .
+  // ylim is a positive float.
+  //----------------------------------------------
+  barchart( container, data, ylim, color) {
+    color = color || 'steelblue'
+    var [d3,$] = [this.d3, this.$]
+    var C = d3.select( container)
+    $(container).html('')
+    var w  = $(container).width()
+    var h = $(container).height()
+
+    var margin = {top: 20, right: 20, bottom: 70, left: 40}
+      ,width = w - margin.left - margin.right
+      ,height = h - margin.top - margin.bottom
+
+    var svg = C.append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+    var scale_x = d3.scaleBand()
+      .domain( data.map( function(d) { return d[0] }))
+      .rangeRound( [0, width])
+      .padding( 0.05)
+
+    var scale_y = d3.scaleLinear()
+      .domain( [0, ylim])
+      .range( [height, 0])
+
+    var xAxis = d3.axisBottom(scale_x)
+      .tickFormat( d3.format( '.3f'))
+
+    var yAxis = d3.axisLeft(scale_y)
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" );
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+
+    svg.selectAll("bar")
+      .data(data)
+      .enter().append("rect")
+      .style("fill", color)
+      .attr("x", function(d) { return scale_x( d[0]) })
+      .attr("width", scale_x.bandwidth())
+      .attr("y", function(d) { return scale_y( d[1]) })
+      .attr("height", function(d) { return height - scale_y( d[1]) })
+
+  } // barchart()
+
+  //-----------------
+  //--- API stuff ---
+  //-----------------
 
   // Hit any endpoint and call completion with result
   //---------------------------------------------------
